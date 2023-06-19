369a9a807b884cefff865cbaf9e4ee8468306b64
Jatin Patil <jatinp8390@gmail.com>
2023-06-19 10:25:55 +0530

    initial setup

---
+++ habitMaker/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <div className='bg-red-500 text-4xl '>
+        hello world
+      </div>
+    </main>
+  )
+}
+
+export default App
